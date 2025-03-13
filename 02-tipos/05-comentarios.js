38a00d49e64cb784e369e9290f77920aeb252129
omar-caiguan-ojeda <leo.caiguan.ojeda@gmail.com>
2025-03-13 11:26:55 -0300

    Comentarios

--- 02-tipos/05-comentarios.js
+++ 02-tipos/05-comentarios.js
@@ -17,5 +17,8 @@ O
 
 let numero = 42;
 
+//? Esto es un comentario de una sola línea
+//! Esto es un comentario de una sola línea con advertencia
+//* Esto es un comentario de una sola línea con información
 
 console.log(numero);
