a5e4ad626c29cb2fdb6ff1b7e64d980081cf22c6
omar-caiguan-ojeda <leo.caiguan.ojeda@gmail.com>
2025-03-13 12:38:19 -0300

    arrays

--- 02-tipos/07-array.js
+++ 02-tipos/07-array.js
@@ -1,3 +1,5 @@
+//* DATO DE REFERENCIA
+// Array = Arreglo = Lista = Colección
 let animales = ["chanchito", "caballo"];
 
 console.log(animales);
@@ -6,6 +8,8 @@ animales[2] = 'dragon';
 console.log(animales);
 
 animales[10] = 'pez';
+animales[2] = 'unicornio';
+console.log(animales);
 console.log(animales[7]);
 console.log(typeof animales);
 
