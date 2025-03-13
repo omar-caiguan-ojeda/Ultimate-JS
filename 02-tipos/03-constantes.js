3458c1e6d92ba34d853cbdacbbfa0c078086fb3e
omar-caiguan-ojeda <leo.caiguan.ojeda@gmail.com>
2025-03-13 11:07:39 -0300

    Constantes

--- 02-tipos/03-constantes.js
+++ 02-tipos/03-constantes.js
@@ -1,4 +1,13 @@
-const nombre = "Hola Mundo";
+//* Las variable LET pueden cambiar su valor
+let saludo = 'Hello World';
+saludo = "Leo"
+
+console.log(saludo);
+
+
+//* En las constante no cabia su valor
+//! Si se intenta cambiar el valor de una constante, se generará un error
+const bienvenida = "Hola Mundo";
 nombre = "chanchito feliz"; // Da ERROR
 
-console.log(nombre);
+console.log(`${bienvenida} ${nombre}`);
