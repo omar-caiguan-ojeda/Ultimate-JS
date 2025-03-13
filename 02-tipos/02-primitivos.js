6e05012f47701e5f9d035a9b46146372a334b28a
omar-caiguan-ojeda <leo.caiguan.ojeda@gmail.com>
2025-03-13 11:01:08 -0300

    Datos primitivos

--- 02-tipos/02-primitivos.js
+++ 02-tipos/02-primitivos.js
@@ -1,5 +1,27 @@
 let numero = 1;
+let n = 2;
+
 let texto = 'Hola Mundo';
+let nombre = 'Omar Leonardo';
+
 let verdadero = true;
 let falso = false;
+
 let noDefinido;
+let undefinedDate
+
+let nulo = null;
+
+console.log("La variable numero es: ", typeof numero)
+console.log("La variable n es: ", typeof n)
+
+console.log("La variable texto es: ", typeof texto)
+console.log("La variable nombre es: ", typeof nombre)
+
+console.log("La variable verdadero es: ", typeof verdadero)
+console.log("La variable falso es: ", typeof falso)
+
+console.log("La variable noDefinido es: ", typeof noDefinido)
+console.log("La variable undefinedDate es: ", typeof undefinedDate)
+
+console.log("La variable nulo es: ", typeof null)
