32602c8a02490f51f5c8539240cf6cb1901df6c8
omar-caiguan-ojeda <leo.caiguan.ojeda@gmail.com>
2025-03-13 11:24:42 -0300

    Tipado dinamico 2

--- 02-tipos/04-tipado-dinamico.js
+++ 02-tipos/04-tipado-dinamico.js
@@ -1,12 +1,13 @@
 //* JavaScript utiliza tipado Dinámico, es decir, no es necesario declarar el tipo de dato de una variable al momento de crearla, el lenguaje automáticamente lo detecta. Ademas de que el tipo de dato de una variable puede cambiar en tiempo de ejecución.
 
 //? Utilizando Typeof
+// SHIFT + ALT = DUPLICA LA LINEA
 
 let numero = 42;
 let nombre = "Hola Mundo";
 let verdadero = true;
 let undef;
-let nula = null;
+let nula = null; // Devuelve object porque lo consideran la ausencia de un objeto.
 
 //nombre = 53;
 
