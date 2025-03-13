9e01526652067ca2ea6ff4682fcf32a172d54c50
omar-caiguan-ojeda <leo.caiguan.ojeda@gmail.com>
2025-03-13 12:45:48 -0300

    function

--- 02-tipos/08-funciones.js
+++ 02-tipos/08-funciones.js
@@ -1,15 +1,16 @@
+//*DATO DE REFERENCIA
+//Funciones
+//Funciones = Métodos = Subrutinas = Procedimientos
+
 function saludar() {
     console.log("Hola Mundo");
 }
-
 saludar();
-
-
 
 function suma() {
     return 2 + 2;
 }
+let resultado = suma();
 
-//let resultado = suma();
-
+console.log(`Otra forma de obtener valor de la función, suma()= ${resultado}`);
 console.log(suma());
